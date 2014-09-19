e5017d84bf00ac25f30d10ce74df476ff0dade0e
jdddog <jamie.diprose@gmail.com>
2014-09-19 23:49:23 +1200

    enumeration changes

--- hri_api/src/hri_api/entities/enumerations.py
+++ hri_api/src/hri_api/entities/enumerations.py
@@ -17,6 +17,12 @@ class IExpression(Enum):
     def default_duration(self):
         raise NotImplementedError('please implement this method')
 
+    def default_speed(self):
+        raise NotImplementedError('please implement this method')
+
+    def default_intensity(self):
+        raise NotImplementedError('please implement this method')
+
     def get_data(self):
         for i in self.data:
             if self is i[0]:
